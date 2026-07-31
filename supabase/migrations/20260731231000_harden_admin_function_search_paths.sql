241c3d7b6eb01021107a257646c21e0312b51a0e
DefaniCo <hello@defani.co>
2026-07-31 22:56:59 +0300

    security: harden admin function search paths

---
+++ supabase/migrations/20260731231000_harden_admin_function_search_paths.sql
@@ -0,0 +1,15 @@
+﻿begin;
+
+alter function public.admin_list_forum_activities(integer)
+set search_path = '';
+
+alter function public.admin_list_topics(integer)
+set search_path = '';
+
+alter function public.admin_list_users()
+set search_path = '';
+
+alter function public.admin_set_topic_status(uuid, text, text)
+set search_path = '';
+
+commit;
